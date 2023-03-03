daf488e7fa7e97c41e60668f3d1451b71ffc388c
sachinsnowflake <126853327+sachinsnowflake@users.noreply.github.com>
2023-03-03 13:17:10 +0530

    Update streamlit_app.py
    Added Menu. Updated the Name

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
-streamlit.text(' Omega 3 & Blueberry Oatmeal')
-streamlit.text(' Kale, Spinach & Rocket Smoothie')
-streamlit.text(' Hard Boiled Free-range Egg')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard Boiled Free-range Egg')
+streamlit.text(' 🥑 Avacado Toast')            
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
